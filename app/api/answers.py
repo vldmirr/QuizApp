1ab4f15bfb968a560b4396e9ad06b8a858e5d1ba
vldmirr <tsutsura18@gmail.com>
2025-11-16 20:44:51 +0300

    replacing used import names

--- app/api/answers.py
+++ app/api/answers.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import Session
 
 from app.db import get_db
 from app.models.schemas import AnswerCreate, AnswerResponse
-from app.service import answers as crud_answers
-from app.service import questions as crud_questions
+from app.service import answers as serviceA
+from app.service import questions as serviceQ
 
 router = APIRouter()
 
@@ -13,16 +13,16 @@ router = APIRouter()
 def create_answer(question_id: int = Path(..., gt=0), answer: AnswerCreate = ...,db: Session = Depends(get_db)):
     """Добавить ответ к вопросу"""
     # Проверяем существование вопроса
-    question = crud_questions.get_question(db, question_id=question_id)
+    question = serviceQ.get_question(db, question_id=question_id)
     if not question:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Question not found")
     
-    return crud_answers.create_answer(db=db, answer=answer, question_id=question_id)
+    return serviceA.create_answer(db=db, answer=answer, question_id=question_id)
 
 @router.get("/answers/{answer_id}", response_model=AnswerResponse)
 def get_answer(answer_id: int, db: Session = Depends(get_db)):
     """Получить конкретный ответ"""
-    db_answer = crud_answers.get_answer(db, answer_id=answer_id)
+    db_answer = serviceA.get_answer(db, answer_id=answer_id)
     if db_answer is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -33,7 +33,7 @@ def get_answer(answer_id: int, db: Session = Depends(get_db)):
 @router.delete("/answers/{answer_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_answer(answer_id: int, db: Session = Depends(get_db)):
     """Удалить ответ"""
-    success = crud_answers.delete_answer(db, answer_id=answer_id)
+    success = serviceA.delete_answer(db, answer_id=answer_id)
     if not success:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
--- app/api/questions.py
+++ app/api/questions.py
@@ -4,25 +4,25 @@ from typing import List
 
 from app.db import get_db
 from app.models.schemas import QuestionCreate, QuestionResponse, QuestionWithAnswers
-from app.service import questions as crud_questions
-from app.service import answers as crud_answers
+from app.service import questions as serviceQ
+
 
 router = APIRouter(prefix="/questions")
 
 @router.get("/", response_model=List[QuestionResponse])
 def get_questions(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     """Получить список всех вопросов"""
-    return crud_questions.get_questions(db, skip=skip, limit=limit)
+    return serviceQ.get_questions(db, skip=skip, limit=limit)
 
 @router.post("/", response_model=QuestionResponse, status_code=status.HTTP_201_CREATED)
 def create_question(question: QuestionCreate, db: Session = Depends(get_db)):
     """Создать новый вопрос"""
-    return crud_questions.create_question(db=db, question=question)
+    return serviceQ.create_question(db=db, question=question)
 
 @router.get("/{question_id}", response_model=QuestionWithAnswers)
 def get_question(question_id: int, db: Session = Depends(get_db)):
     """Получить вопрос и все ответы на него"""
-    db_question = crud_questions.get_question_with_answers(db, question_id=question_id)
+    db_question = serviceQ.get_question_with_answers(db, question_id=question_id)
     if db_question is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -33,7 +33,7 @@ def get_question(question_id: int, db: Session = Depends(get_db)):
 @router.delete("/{question_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_question(question_id: int, db: Session = Depends(get_db)):
     """Удалить вопрос (вместе с ответами)"""
-    success = crud_questions.delete_question(db, question_id=question_id)
+    success = serviceQ.delete_question(db, question_id=question_id)
     if not success:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
