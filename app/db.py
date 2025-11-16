713606412ca5bbb7aa41a823fbaa9c946209a25b
vldmirr <tsutsura18@gmail.com>
2025-11-16 22:27:14 +0300

    добавил использование конфигурации через .env

--- app/db.py
+++ app/db.py
@@ -1,12 +1,27 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL", 
-    "postgresql://postgres:password@localhost:5432/quizapp_db"
-)
+load_dotenv()
+
+
+def get_database_url():
+    # те которые задали специально в .env
+    database_url = os.getenv("DATABASE_URL")
+    if database_url:
+        return database_url
+    
+    # дефолтные
+    user = os.getenv("POSTGRES_USER", "postgres")
+    password = os.getenv("POSTGRES_PASSWORD", "password")
+    host = os.getenv("POSTGRES_HOST", "db")
+    port = os.getenv("POSTGRES_PORT", "5432")
+    db_name = os.getenv("POSTGRES_DB", "qna_db")
+    
+    return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+
+DATABASE_URL = get_database_url()
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
