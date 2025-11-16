e02626caba455e369de636a6b0efce1c3b3e70b2
vldmirr <tsutsura18@gmail.com>
2025-11-16 14:07:24 +0300

    fix path declative_base

--- app/models/models.py
+++ app/models/models.py
@@ -1,8 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-import uuid
 
 Base = declarative_base()
 
